34422167479afdae140943c91391e0bac205eb68
Ivan S <steblivec1000@gmail.com>
2023-12-23 19:04:38 +0300

    unnamed fixes

--- lib/presentation/screens/friend_screen/friend_screen.dart
+++ lib/presentation/screens/friend_screen/friend_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
 import 'package:rave_flock/presentation/bloc/friends_data_bloc/friends_data_event.dart';
@@ -33,43 +34,116 @@ class _FriendScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            IconButton(
-                onPressed: () {
-                  context.pop();
-                  GetIt.I<FriendsDataBloc>().add(FriendsDataEvent.removeFriend(
-                      AuthService.getUserId() ?? '', friendId));
+    return Container(
+      decoration: BoxDecoration(
+          gradient: RadialGradient(
+              radius: 1.2.r, colors: [Color(0xFF5B1828), Colors.black])),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(),
+        body: SafeArea(
+          child: BlocBuilder<FriendsDataBloc, FriendsDataState>(
+            builder: (context, state) {
+              return state.when(
+                init: () {
+                  return const CircularProgressIndicator(); // TODO:
                 },
-                icon: const Icon(
-                  Icons.remove_circle_outline_outlined,
-                  color: Colors.red,
-                )),
-            BlocBuilder<FriendsDataBloc, FriendsDataState>(
-              buildWhen: (prev, curr) {
-                if (prev == curr) {
-                  return false;
-                }
-                return true;
-              },
-              builder: (context, state) {
-                return state.when(
-                  init: () {
-                    return const CircularProgressIndicator(); // TODO:
-                  },
-                  loaded: (friends) {
-                    UserModel friendUserModel = friends
-                        .firstWhere((element) => element.userId == friendId);
-                    return Center(child: Text('$friendUserModel'));
-                  },
-                  error: (e) => ErrorScreen(error: e),
-                );
-              },
-            ),
-          ],
+                loaded: (friends) {
+                  UserModel friendUserModel = friends.firstWhere((element) => element.userId == friendId);
+                  return Column(
+                    children: [
+                      friendUserModel.avatarUrl != null ? CircleAvatar(backgroundImage: NetworkImage(friendUserModel.avatarUrl!),radius: 130.r,): CircleAvatar(backgroundColor: Colors.blue,radius: 130.r,),
+                      SizedBox(height: 20.h,),
+                      Text(friendUserModel.username.toString()),
+                      if (friendUserModel.nickname != null) Text(friendUserModel.nickname.toString()),
+                      SizedBox(height: 20.h,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Column(
+                            children: [
+                              Container(
+                                width: 66.w,
+                                height: 51.h,
+                                decoration: ShapeDecoration(
+                                  color: Color(0x1EB71B1B),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(28),
+                                  ),
+                                ),
+                                child: const Center(child: Text('##')),
+                              ),
+                              Text('FRIENDS')
+                            ],
+                          ),
+                          SizedBox(width: 190.w,),
+                          Column(
+                            children: [
+                              Container(
+                                width: 66.w,
+                                height: 51.h,
+                                decoration: ShapeDecoration(
+                                  color: Color(0x1EB71B1B),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(28),
+                                  ),
+                                ),
+                                child: const Center(child: Text('##')),
+                              ),
+                              Text('RAVES')
+                            ],
+                          ),
+
+                        ],
+                      ),
+                      SizedBox(height: 28.h,),
+
+                      GestureDetector(
+                        onTap: () {
+                          // TODO:
+                        },
+                        child: Container(
+                          width: 241.w,
+                          height: 40.h,
+                          decoration: ShapeDecoration(
+                            color: Color(0x1EB71B1B),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(28),
+                            ),
+                          ),
+                          child: const Center(child: Text('INVITE TO RAVE')),
+                        ),
+                      ),
+                      SizedBox(height: 28.h,),
+                      GestureDetector(
+                        onTap: () {
+                          context.pop();
+                          GetIt.I<FriendsDataBloc>().add(
+                              FriendsDataEvent.removeFriend(AuthService.getUserId() ?? '', friendId));
+                        },
+                        child: Container(
+                          width: 241.w,
+                          height: 40.h,
+                          decoration: ShapeDecoration(
+                            color: Color(0x1EB71B1B),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(28),
+                            ),
+                          ),
+                          child: const Center(child: Text('DELETE')),
+                        ),
+                      ),
+                      SizedBox(height: 28.h,),
+
+                     // Center(child: Text('$friendUserModel'))
+
+                    ],
+                  );
+                },
+                error: (e) => ErrorScreen(error: e),
+              );
+            },
+          ),
         ),
       ),
     );
